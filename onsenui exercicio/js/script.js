be57bd7a1ed3aa127398b025c059c244e914c8a4
Joao Gouveia <joaogouveia.tc24@gmail.com>
2019-10-03 21:10:32 +0100

    finish onsen ui exercises

--- onsenui exercicio/js/script.js
+++ onsenui exercicio/js/script.js
@@ -1,9 +1,9 @@
-document.addEventListener('prechange', function (event) {
+document.addEventListener('prechange', function(event) {
     document.querySelector('ons-toolbar .center')
         .innerHTML = event.tabItem.getAttribute('label');
 });
 
-var adicionar = function () {
+var adicionar = function() {
     var name = document.getElementById("nome");
     if (name.value.length == 0) {
         ons.notification.alert('Peencha primeiro o nome!');
@@ -11,6 +11,7 @@ var adicionar = function () {
         ons.notification.toast('Sucesso!', { timeout: 2000 });
         var list = document.querySelector('ons-list');
         var newItem = document.createElement('ons-list-item');
+        newItem.setAttribute('class', 'list-item');
         var newItemDiv = document.createElement('div');
         newItemDiv.innerHTML += name.value;
         newItemDiv.setAttribute('class', 'center list-item__center');
@@ -20,24 +21,47 @@ var adicionar = function () {
     }
 }
 
-var enviar = function () {
+var enviar = function() {
     var nome = document.getElementById("name").value;
     var email = document.getElementById("email").value;
     var message = document.getElementById("message").value;
-    if(!nome || !email || !message){
+    if (!nome || !email || !message) {
         ons.notification.alert('Faltam preencher dados!!');
-    }else{
+    } else {
+        ons.notification.toast('Mensagem Enviada!', { timeout: 2000 });
         var list = document.getElementById("messageList");
         var newItem = document.createElement("ons-list-item");
+        newItem.setAttribute('class', 'list-item');
+        newItemDiv = document.createElement('div');
+        newItemDivP = document.createElement('p');
+        newItemDiv.setAttribute('class', 'center list-item_center');
+        newItemDivP.innerHTML += nome + ", " + email + ": " + message;
+        newItem.appendChild(newItemDiv);
+        newItemDiv.appendChild(newItemDivP);
+        list.appendChild(newItem);
+        document.getElementById('name').value = "";
+        document.getElementById('email').value = "";
+        document.getElementById('message').value = "";
     }
 }
 
-ons.ready(function () {
-    var pageChange = document.getElementById("btnChange").addEventListener('click', function () {
+ons.ready(function() {
+    var pageChange = document.getElementById("btnChange").addEventListener('click', function() {
         document.querySelector('ons-tabbar').setActiveTab(1);
     });
 
-    var listClick = document.querySelector('ons-list').addEventListener('click', function () {
-        document.querySelector('ons-tabbar').setActiveTab(3);
-    });
+    var list = document.querySelectorAll('ons-list > ons-list-item > div');
+    list.forEach(function(element) {
+        element.addEventListener('click', function(e) {
+            myNavigator.pushPage('itemNav.html', { data: { item: e.target.textContent } });
+        })
+    })
+});
+
+document.addEventListener('init', function(event) {
+    if (event.target.id === 'itemNav') {
+        var title = 'Item Page';
+        event.target.querySelector('ons-toolbar div.center').textContent = title;
+        event.target.querySelector('p').textContent = "You have clicked on: " + event.target.data.item;
+    }
 });
